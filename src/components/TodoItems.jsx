50ed0745adb805ba33735f2994916df8e89c0349
carl pinto <carlpinto25@gmail.com>
2025-08-04 18:27:49 +0530

    Update TodoItems.jsx

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,7 +7,7 @@ import not_tick from '../assets/not_tick.png'
 const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-     {/* CHANGED: Corrected function call from 'Toggle' to 'toggle' to match the prop name. */}
+
      <div onClick={()=>{toggle(id)}} className='flex flex-1 item-center cursor-pointer'> 
         <img src={isComplete ? tick : not_tick} alt="tick" className='w-6 h-6' />
         <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>{text}</p>
